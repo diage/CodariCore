c968586acbafe16ab8eb40e626aa3b99d332baeb
Mhenlo <mphils55@gmail.com>
2013-11-17 15:21:42 -0500

    Deleted SkillCore(unnecessary class). Finished implementing RoleCore and SkillListener

--- src/com/codari/arenacore/players/role/RoleCore.java
+++ src/com/codari/arenacore/players/role/RoleCore.java
@@ -1,59 +1,116 @@
 package com.codari.arenacore.players.role;
 
 import java.util.HashMap;
+
 import java.util.Map;
 
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.codari.api5.Codari;
 import com.codari.arena5.players.role.Role;
 import com.codari.arena5.players.skills.Skill;
 import com.codari.arena5.players.skills.SkillActivation;
 
 public class RoleCore implements Role {
 	//-----Fields-----//
+	private int maxCooldown = 20; 	//(in seconds)
+	private int cooldown = maxCooldown;
+	private boolean onCooldown = false;
+	private String name;
+
 	private Map<SkillActivation, Skill> skills;
 	
-	public RoleCore(Skill...skills) {
+	public RoleCore(String name) {
+		this.name = name;
+	}
+
+	public RoleCore(String name, Skill...skills) {
+		this.name = name;
 		this.skills = new HashMap<>();
 		for(Skill skill : skills) {
 			this.skills.put(skill.getSkillActivation(), skill);
 		}
 	}
-	
+
+	public RoleCore(String name, int maxCooldown, Skill...skills) {
+		this.name = name;
+		this.maxCooldown = maxCooldown;
+		this.skills = new HashMap<>();
+		for(Skill skill : skills) {
+			this.skills.put(skill.getSkillActivation(), skill);
+		}
+	}
+
 	@Override
 	public Skill[] getSkills() {
 		return (Skill[]) this.skills.values().toArray();
 	}
 
+	/* Checks if the skill is on cooldown first. If the skill's not on cooldown, checks for the Key with contains double jump,
+	 * activates the skill, then puts the skill on cooldown. */
 	@Override
 	public void doubleJump() {
-		if(this.skills.containsKey(SkillActivation.DOUBLE_JUMP)) {
-			this.skills.get(SkillActivation.DOUBLE_JUMP).activateSkill(); 
-		}		
+		if(this.checkCooldown()){
+			if(this.skills.containsKey(SkillActivation.DOUBLE_JUMP)) {
+				this.skills.get(SkillActivation.DOUBLE_JUMP).activateSkill(); 
+				this.onCooldown();
+			}	
+		}
 	}
 
 	@Override
 	public void block() {
-		if(this.skills.containsKey(SkillActivation.BLOCK)) {
-			this.skills.get(SkillActivation.BLOCK).activateSkill(); 
+		if(this.checkCooldown()){
+			if(this.skills.containsKey(SkillActivation.BLOCK)) {
+				this.skills.get(SkillActivation.BLOCK).activateSkill(); 
+				this.onCooldown();
+			}
 		}
 	}
 
 	@Override
 	public void sprint() {
-		if(this.skills.containsKey(SkillActivation.SPRINT)) {
-			this.skills.get(SkillActivation.SPRINT).activateSkill(); 
-		}	
+		if(this.checkCooldown()){
+			if(this.skills.containsKey(SkillActivation.SPRINT)) {
+				this.skills.get(SkillActivation.SPRINT).activateSkill(); 
+				this.onCooldown();
+			}	
+		}
 	}
 
 	@Override
 	public void sneak() {
-		if(this.skills.containsKey(SkillActivation.SNEAK)) {
-			this.skills.get(SkillActivation.SNEAK).activateSkill(); 
+		if(this.checkCooldown()){
+			if(this.skills.containsKey(SkillActivation.SNEAK)) {
+				this.skills.get(SkillActivation.SNEAK).activateSkill(); 
+				this.onCooldown();
+			}
 		}
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.name;
+	}
+
+	private boolean checkCooldown() {
+		return this.onCooldown;
+	}
+
+	private void onCooldown() {
+		this.onCooldown = true;
+		BukkitRunnable runner = new BukkitRunnable() {
+			@Override
+			public void run() {
+				cooldown--;
+				if(cooldown == 0) {
+					cooldown = maxCooldown;
+					onCooldown = false;
+					super.cancel();
+				}
+			}
+		};
+
+		runner.runTaskTimer(Codari.INSTANCE, 0, 20);		
 	}
 }
--- src/com/codari/arenacore/players/skills/SkillListener.java
+++ src/com/codari/arenacore/players/skills/SkillListener.java
@@ -1,42 +1,42 @@
 package com.codari.arenacore.players.skills;
 
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.event.player.PlayerToggleSprintEvent;
 
 import com.codari.api5.Codari;
-import com.codari.arena5.players.combatants.Combatant;
-import com.codari.arena5.players.role.Role;
 
 public class SkillListener implements Listener {
 	@EventHandler()
 	public void triggerDoubleJump(PlayerToggleFlightEvent e) {
-		Combatant combatant = Codari.INSTANCE.getArenaManager().getCombatant(e.getPlayer());
-		@SuppressWarnings("unused")
-		Role role = combatant.getRole();
+		if(!(e.getPlayer().getGameMode().equals(GameMode.CREATIVE))) {
+			e.getPlayer().setFlying(false);
+			Codari.INSTANCE.getArenaManager().getCombatant(e.getPlayer()).getRole().doubleJump();
+		}
 	}
-	
-	//Gotta make my own event to check when a player blocks
+
 	@EventHandler()
-	public void triggerBlock(PlayerToggleFlightEvent e) {
-		Combatant combatant = Codari.INSTANCE.getArenaManager().getCombatant(e.getPlayer());
-		@SuppressWarnings("unused")
-		Role role = combatant.getRole();
+	public void triggerBlock(PlayerInteractEvent e) {
+		if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+			if(e.getPlayer().isBlocking()) {
+				Codari.INSTANCE.getArenaManager().getCombatant(e.getPlayer()).getRole().block();				
+			}
+		}
 	}
-	
-	@SuppressWarnings("unused")
+
 	@EventHandler()
 	public void triggerSprint(PlayerToggleSprintEvent e) {
-		Combatant combatant = Codari.INSTANCE.getArenaManager().getCombatant(e.getPlayer());
-		Role role = combatant.getRole();
+		e.getPlayer().setSprinting(false);
+		Codari.INSTANCE.getArenaManager().getCombatant(e.getPlayer()).getRole().sneak();
 	}
-	
+
 	@EventHandler()
 	public void triggerSneak(PlayerToggleSneakEvent e) {
-		Combatant combatant = Codari.INSTANCE.getArenaManager().getCombatant(e.getPlayer());
-		@SuppressWarnings("unused")
-		Role role = combatant.getRole();
+		Codari.INSTANCE.getArenaManager().getCombatant(e.getPlayer()).getRole().sneak();
 	}
 }
