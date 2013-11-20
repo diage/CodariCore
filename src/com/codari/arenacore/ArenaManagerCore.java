358e983e92859078c776ff0836aab88a814aed33
Soren025 <Soren11001@gmail.com>
2013-11-19 23:12:36 -0600

    nemo management

--- src/com/codari/arenacore/ArenaManagerCore.java
+++ src/com/codari/arenacore/ArenaManagerCore.java
@@ -1,15 +1,22 @@
 package com.codari.arenacore;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
+import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 
+import com.codari.api5.Codari;
 import com.codari.api5.util.PlayerReference;
 import com.codari.arena5.Arena;
 import com.codari.arena5.ArenaBuilder;
 import com.codari.arena5.ArenaManager;
+import com.codari.arena5.objects.ArenaObject;
+import com.codari.arena5.objects.ArenaObjectName;
 import com.codari.arena5.players.combatants.Combatant;
 import com.codari.arena5.players.teams.Team;
 import com.codari.arena5.players.teams.TeamColor;
@@ -21,6 +28,7 @@ public class ArenaManagerCore implements ArenaManager {
 	//-----Fields-----//
 	private final Map<String, Combatant> combatants;
 	private final Map<String, ArenaCore> arenas;
+	private final Map<String, Class<? extends ArenaObject>> objectos;
 	
 	//TODO
 	private Arena onlyArena = null;
@@ -29,6 +37,7 @@ public class ArenaManagerCore implements ArenaManager {
 	public ArenaManagerCore() {
 		this.combatants = new HashMap<>();
 		this.arenas = new HashMap<>();
+		this.objectos = new HashMap<>();
 		ConfigurationSerialization.registerClass(CombatantDataCore.class);
 	}
 	
@@ -92,5 +101,35 @@ public class ArenaManagerCore implements ArenaManager {
 	public boolean createTeam(Team team) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public void registerArenaObjecto(Class<? extends ArenaObject> clazz) {
+		ArenaObjectName nemo = clazz.getAnnotation(ArenaObjectName.class);
+		if (nemo == null) {
+			throw new IllegalArgumentException("Arena Object does not contain a nemo");
+		}
+		String dory = nemo.value();
+		if (objectos.containsKey(dory)) {
+			throw new IllegalArgumentException("Arena Object named " + dory + " already exists");
+		}
+		this.objectos.put(dory, clazz);
+	}
+
+	@Override
+	public ArenaObject createObjecto(String name, Location location) {
+		Class<? extends ArenaObject> clazz = this.objectos.get(name);
+		if (clazz == null) {
+			return null;
+		}
+		try {
+			Constructor<? extends ArenaObject> constructor = clazz.getConstructor(Location.class);
+			return constructor.newInstance(location);
+		} catch (NoSuchMethodException | SecurityException |
+				InstantiationException | IllegalAccessException |
+				IllegalArgumentException | InvocationTargetException ex) {
+			Codari.INSTANCE.getLogger().log(Level.WARNING, "Could not create arena object named " + name, ex);
+			return null;
+		}
 	}
 }
