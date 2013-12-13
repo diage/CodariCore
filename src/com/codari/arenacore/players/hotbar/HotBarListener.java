3a2787ac42e7a67645a40e05561dee8877dfdd68
Soren025 <Soren11001@gmail.com>
2013-12-12 22:14:04 -0600

    hotbar

--- src/com/codari/arenacore/players/hotbar/HotBarListener.java
+++ src/com/codari/arenacore/players/hotbar/HotBarListener.java
@@ -1,21 +1,43 @@
 package com.codari.arenacore.players.hotbar;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 
+import com.codari.api5.Codari;
+import com.codari.api5.util.scheduler.BukkitTime;
+import com.codari.arena5.players.combatants.Combatant;
+import com.codari.arena5.players.hotbar.HotbarSelectEvent;
+import com.codari.arena5.players.hotbar.HotbarSlot;
+
 public class HotBarListener implements Listener {
+	//-----Constants-----//
+	private final static int DEFAULT_SLOT = 7;
+	
 	@EventHandler(priority = EventPriority.MONITOR)
 	private void hotbarSelect(PlayerItemHeldEvent e) {
-		e.getPlayer().getInventory().setHeldItemSlot(7);
-		e.getPlayer().sendMessage(String.valueOf(e.getNewSlot()));
+		Combatant combatant = Codari.getArenaManager().getCombatant(e.getPlayer());
+		if (combatant.isHotbarActive()) {
+			e.getPlayer().getInventory().setHeldItemSlot(DEFAULT_SLOT);
+			if (e.getPreviousSlot() == DEFAULT_SLOT && !combatant.isHotbarOnCooldown()) {
+				int slotSelection = e.getNewSlot();
+				if (slotSelection >= 0 && slotSelection < 6) {
+					HotbarSlot slot = HotbarSlot.values()[slotSelection];
+					Bukkit.getPluginManager().callEvent(new HotbarSelectEvent(combatant, slot,
+							combatant.getPlayer().getInventory().getItem(slotSelection)));
+					combatant.setHotbarCooldown(BukkitTime.SECOND.tickValue(5));
+				}
+			}
+		}
 	}
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	private void hotbarDisable(PlayerInteractEvent e) {
-		if (e.getPlayer().getInventory().getHeldItemSlot() != 7) {
+		Combatant combatant = Codari.getArenaManager().getCombatant(e.getPlayer());
+		if (combatant.isHotbarActive() && e.getPlayer().getInventory().getHeldItemSlot() != 7) {
 			e.setCancelled(true);
 		}
 	}
