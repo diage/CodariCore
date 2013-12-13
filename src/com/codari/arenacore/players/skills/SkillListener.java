d58eb5fd989058dbb0e8ddf15e635786abee51c8
Diage <farina.1919@gmail.com>
2013-12-12 19:45:13 -0500

    asd

--- src/com/codari/arenacore/players/skills/SkillListener.java
+++ src/com/codari/arenacore/players/skills/SkillListener.java
@@ -4,7 +4,6 @@ import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -13,7 +12,7 @@ import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.event.player.PlayerToggleSprintEvent;
 
-import com.codari.api5.CodariI;
+import com.codari.api5.Codari;
 import com.codari.arena5.players.combatants.Combatant;
 import com.codari.arenacore.players.combatants.CombatantCore;
 
@@ -22,7 +21,7 @@ public class SkillListener implements Listener {
 	public void triggerDoubleJump(PlayerToggleFlightEvent e) {
 		if(!(e.getPlayer().getGameMode().equals(GameMode.CREATIVE))) {
 			e.getPlayer().setFlying(false);
-			Combatant combatant = CodariI.INSTANCE.getArenaManager().getCombatant(e.getPlayer());
+			Combatant combatant = Codari.getArenaManager().getCombatant(e.getPlayer());
 			((CombatantCore)combatant).doubleJump(combatant);
 		}
 	}
@@ -30,8 +29,8 @@ public class SkillListener implements Listener {
 	@EventHandler()
 	public void triggerBlock(PlayerInteractEvent e) {
 		if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) || e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-			if(this.checkIfSword(e.getItem().getType())) {
-				Combatant combatant = CodariI.INSTANCE.getArenaManager().getCombatant(e.getPlayer());
+			if(this.checkIfWeapon(e.getItem().getType())) {
+				Combatant combatant = Codari.getArenaManager().getCombatant(e.getPlayer());
 				((CombatantCore)combatant).block(combatant);				
 			}
 		}
@@ -39,36 +38,24 @@ public class SkillListener implements Listener {
 
 	@EventHandler()
 	public void triggerSprint(PlayerToggleSprintEvent e) {
-		Combatant combatant = CodariI.INSTANCE.getArenaManager().getCombatant(e.getPlayer());
+		Combatant combatant = Codari.getArenaManager().getCombatant(e.getPlayer());
 		((CombatantCore)combatant).sprint(combatant);
-	}
-	
-	@EventHandler(priority = EventPriority.MONITOR)
-	public void triggerCancelSprint(PlayerToggleSprintEvent e) {
-		e.getPlayer().setSprinting(false);
 	}
 	
 	@EventHandler()
 	public void playerMoveEvent(PlayerMoveEvent e) {
-		e.getPlayer().setWalkSpeed(e.getPlayer().isSprinting() ? 0.2f : 0.2f);
+		if(e.getPlayer().getGameMode().equals(GameMode.SURVIVAL)) {
+			e.getPlayer().setWalkSpeed(e.getPlayer().isSprinting() ? 0.2f : 0.2f);
+		}
 	}
 
 	@EventHandler()
 	public void triggerSneak(PlayerToggleSneakEvent e) {
-		Combatant combatant = CodariI.INSTANCE.getArenaManager().getCombatant(e.getPlayer());
+		Combatant combatant = Codari.getArenaManager().getCombatant(e.getPlayer());
 		((CombatantCore)combatant).sneak(combatant);
 	}
 	
-	private boolean checkIfSword(Material material) {
-		return EnchantmentTarget.WEAPON.includes(material);
-//		switch(material) {
-//		case WOOD_SWORD:
-//		case IRON_SWORD: 
-//		case GOLD_SWORD:
-//		case DIAMOND_SWORD:
-//			return true;
-//		default:
-//			return false;
-///		}
+	private boolean checkIfWeapon(Material material) {
+		return EnchantmentTarget.WEAPON.includes(material) || EnchantmentTarget.BOW.includes(material);
 	}
 }
