c8e095ba98ca0b04894048739699a9ebf1665b36
Soren025 <Soren11001@gmail.com>
2013-12-30 03:53:12 -0600

    TOOL BAR STUFF

--- src/com/codari/arenacore/players/builders/kit/ToolBarListener.java
+++ src/com/codari/arenacore/players/builders/kit/ToolBarListener.java
@@ -1,5 +1,7 @@
 package com.codari.arenacore.players.builders.kit;
 
+import java.util.List;
+
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -41,6 +43,7 @@ public class ToolBarListener implements Listener {
 			if (kitManager.isToolBarEnabled()) {
 				Kit kit = kitManager.getToolbarKit();
 				String objectName = e.getItem().getItemMeta().getDisplayName();
+				List<String> extraInformation = e.getItem().getItemMeta().getLore();
 				Location location = e.getClickedBlock().getLocation();
 				//Need arena objects converted to accept location
 				//Filter what type of object and stuff
