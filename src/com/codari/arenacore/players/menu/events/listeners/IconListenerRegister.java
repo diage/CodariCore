1afd69448a160d267e8ceee58e13d53cfd44e363
Mhenlo <mphils55@gmail.com>
2014-01-02 14:08:26 -0500

    Added the Kit Listener to the IconListenerRegister class

--- src/com/codari/arenacore/players/menu/events/listeners/IconListenerRegister.java
+++ src/com/codari/arenacore/players/menu/events/listeners/IconListenerRegister.java
@@ -3,6 +3,7 @@ package com.codari.arenacore.players.menu.events.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
+import com.codari.arenacore.players.builders.kit.KitListener;
 import com.codari.arenacore.players.builders.kit.ToolBarListener;
 
 public class IconListenerRegister {
@@ -10,5 +11,6 @@ public class IconListenerRegister {
 	public IconListenerRegister(Plugin plugin) {
 		Bukkit.getPluginManager().registerEvents(new ToolBarListener(), plugin);
 		Bukkit.getPluginManager().registerEvents(new IconListener(), plugin);
+		Bukkit.getPluginManager().registerEvents(new KitListener(), plugin);
 	}
 }
