67070c3e4bda66f9b89065cbfbad1b4a6514b91b
Mhenlo <mphils55@gmail.com>
2014-01-08 20:41:49 -0500

    bugfix

--- src/com/codari/arenacore/players/menu/menus/menustore/teams/InitialTeamOptions.java
+++ src/com/codari/arenacore/players/menu/menus/menustore/teams/InitialTeamOptions.java
@@ -1,7 +1,5 @@
 package com.codari.arenacore.players.menu.menus.menustore.teams;
 
-import org.bukkit.Bukkit;
-
 import com.codari.arena5.players.combatants.Combatant;
 import com.codari.arenacore.players.combatants.CombatantCore;
 import com.codari.arenacore.players.menu.icons.iconstore.common.BackIcon;
@@ -23,13 +21,14 @@ public class InitialTeamOptions extends FunctionMenu {
 		super.setSlot(FunctionMenuSlot.A_TWO, new LeaveTeamIcon(combatant, this));
 	}
 	
+	@SuppressWarnings("deprecation")
 	public void setNoTeamIcon(Combatant combatant) {
 		if(super.hasSlot(FunctionMenuSlot.A_TWO)) {
-			Bukkit.broadcastMessage("Removing icon!"); //TODO
 			((CombatantCore) combatant).getMenuManager().removeMenuSlot(FunctionMenuSlot.A_TWO);
 		} 
 		
 		super.setSlot(FunctionMenuSlot.A_ONE, new CreateTeamMenuIcon(combatant, new TeamCreate(combatant, this, new BackIcon(combatant, this))));
+		combatant.getPlayer().updateInventory();
 	}
 	
 	private void addIcons(Combatant combatant) {
