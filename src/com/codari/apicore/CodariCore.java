d50d863c91d73d15f3bae3726dca278b31ac5658
Soren025 <Soren11001@gmail.com>
2014-01-21 10:50:32 -0600

    asd

--- src/com/codari/apicore/CodariCore.java
+++ src/com/codari/apicore/CodariCore.java
@@ -136,6 +136,7 @@ public final class CodariCore extends JavaPlugin implements CodariI {
 	public void onDisable() {
 		this.setInstanceAccess(false);
 		this.enchantmentManager.unregisterCustomEnchantments();
+		this.arenaManager.saveArenaBuilders();
 	}
 	
 	//-----Public Methods-----//
