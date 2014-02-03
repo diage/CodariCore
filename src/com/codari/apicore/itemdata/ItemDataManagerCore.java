f9d33ff5c3743e3d0aaca6a53a845445b75cbb0a
Soren025 <Soren11001@gmail.com>
2014-02-03 16:25:08 -0600

    sdfa

--- src/com/codari/apicore/itemdata/ItemDataManagerCore.java
+++ src/com/codari/apicore/itemdata/ItemDataManagerCore.java
@@ -1,15 +1,21 @@
 package com.codari.apicore.itemdata;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.codari.api5.itemdata.ItemDataManager;
 
 public class ItemDataManagerCore implements ItemDataManager {
 	//-----Fields-----//
+	private final UUID sessionId = UUID.randomUUID();
 	private final AtomicInteger idGenerator;
+	private final Map<Integer, ItemDataCore> data;
 	
 	//-----Constructor-----//
 	public ItemDataManagerCore() {
 		this.idGenerator = new AtomicInteger(1);
+		this.data = new HashMap<>();
 	}
 }
